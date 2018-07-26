2d3202fcc6903a615762f0712bc00e7f58ec00c8
prolic <saschaprolic@googlemail.com>
2018-07-26 19:59:20 +0800

    finish tests for subscribe_should

--- tests/subscribe_should.php
+++ tests/subscribe_should.php
@@ -17,20 +17,22 @@ use Amp\Promise;
 use Amp\Success;
 use Amp\TimeoutException;
 use PHPUnit\Framework\TestCase;
+use Prooph\EventStoreClient\EventStoreSubscription;
 use Prooph\EventStoreClient\ExpectedVersion;
 use ProophTest\EventStoreClient\Helper\Connection;
 use ProophTest\EventStoreClient\Helper\TestEvent;
 use function Amp\call;
 use function Amp\Promise\timeout;
+use function Amp\Promise\wait;
 
 class subscribe_should extends TestCase
 {
-    private const Timeout = 10000;
+    private const Timeout = 5000;
 
     /** @test */
     public function be_able_to_subscribe_to_non_existing_stream_and_then_catch_new_event(): void
     {
-        Promise\wait(call(function () {
+        wait(call(function () {
             $stream = 'subscribe_should_be_able_to_subscribe_to_non_existing_stream_and_then_catch_created_event';
 
             $appeared = new Deferred();
@@ -56,8 +58,128 @@ class subscribe_should extends TestCase
                 $this->assertTrue($result);
             } catch (TimeoutException $e) {
                 $this->fail('Appeared countdown event timed out');
+            }
 
-                return;
+            $connection->close();
+        }));
+    }
+
+    /** @test */
+    public function allow_multiple_subscriptions_to_same_stream(): void
+    {
+        wait(call(function () {
+            $stream = 'subscribe_should_allow_multiple_subscriptions_to_same_stream';
+
+            $connection = Connection::createAsync();
+
+            yield $connection->connectAsync();
+
+            $appeared1 = new Deferred();
+            $appeared2 = new Deferred();
+
+            yield $connection->subscribeToStreamAsync(
+                $stream,
+                false,
+                function () use ($appeared1): Promise {
+                    $appeared1->resolve(true);
+
+                    return new Success();
+                }
+            );
+
+            yield $connection->subscribeToStreamAsync(
+                $stream,
+                false,
+                function () use ($appeared2): Promise {
+                    $appeared2->resolve(true);
+
+                    return new Success();
+                }
+            );
+
+            $connection->appendToStreamAsync($stream, ExpectedVersion::EmptyStream, [TestEvent::new()]);
+
+            foreach ([$appeared1, $appeared2] as $appeared) {
+                try {
+                    $result = yield timeout($appeared->promise(), self::Timeout);
+                    $this->assertTrue($result);
+                } catch (TimeoutException $e) {
+                    $this->fail('Appeared countdown event timed out');
+                }
+            }
+
+            $connection->close();
+        }));
+    }
+
+    /** @test */
+    public function call_dropped_callback_after_unsubscribe_method_call(): void
+    {
+        wait(call(function () {
+            $stream = 'subscribe_should_call_dropped_callback_after_unsubscribe_method_call';
+
+            $connection = Connection::createAsync();
+
+            yield $connection->connectAsync();
+
+            $dropped = new Deferred();
+
+            /** @var EventStoreSubscription $subscription */
+            $subscription = yield $connection->subscribeToStreamAsync(
+                $stream,
+                false,
+                function (): Promise {
+                    return new Success();
+                },
+                function () use ($dropped): void {
+                    $dropped->resolve(true);
+                }
+
+            );
+
+            $subscription->unsubscribe();
+
+            try {
+                $result = yield timeout($dropped->promise(), self::Timeout);
+                $this->assertTrue($result);
+            } catch (TimeoutException $e) {
+                $this->fail('Dropdown countdown event timed out');
+            }
+
+            $connection->close();
+        }));
+    }
+
+    /** @test */
+    public function catch_deleted_events_as_well(): void
+    {
+        wait(call(function () {
+            $stream = 'subscribe_should_catch_created_and_deleted_events_as_well';
+
+            $connection = Connection::createAsync();
+
+            yield $connection->connectAsync();
+
+            $appeared = new Deferred();
+
+            /** @var EventStoreSubscription $subscription */
+            $subscription = yield $connection->subscribeToStreamAsync(
+                $stream,
+                false,
+                function () use ($appeared): Promise {
+                    $appeared->resolve(true);
+
+                    return new Success();
+                }
+            );
+
+            yield $connection->deleteStreamAsync($stream, ExpectedVersion::EmptyStream, true);
+
+            try {
+                $result = yield timeout($appeared->promise(), self::Timeout);
+                $this->assertTrue($result);
+            } catch (TimeoutException $e) {
+                $this->fail('Appeared countdown event timed out');
             }
 
             $connection->close();
