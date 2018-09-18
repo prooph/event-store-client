0a86652f88cae05dac80180f023f447d0500b748
prolic <saschaprolic@googlemail.com>
2018-09-19 00:56:21 +0800

    add new tests

---
+++ tests/create_duplicate_persistent_subscription_group.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of the prooph/event-store-client.
+ * (c) 2018-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2018-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStoreClient;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+use Prooph\EventStoreClient\Exception\InvalidOperationException;
+use Prooph\EventStoreClient\Internal\UuidGenerator;
+use Prooph\EventStoreClient\PersistentSubscriptionSettings;
+use Throwable;
+
+class create_duplicate_persistent_subscription_group extends TestCase
+{
+    use SpecificationWithConnection;
+
+    /** @var string */
+    private $stream;
+    /** @var PersistentSubscriptionSettings */
+    private $settings;
+
+    protected function setUp(): void
+    {
+        $this->stream = UuidGenerator::generate();
+        $this->settings = PersistentSubscriptionSettings::create()
+            ->doNotResolveLinkTos()
+            ->startFromCurrent()
+            ->build();
+    }
+
+    protected function when(): Generator
+    {
+        yield $this->conn->createPersistentSubscriptionAsync(
+            $this->stream,
+            'group32',
+            $this->settings,
+            DefaultData::adminCredentials()
+        );
+    }
+
+    /**
+     * @test
+     * @throws Throwable
+     */
+    public function the_completion_fails_with_invalid_operation_exception(): void
+    {
+        $this->executeCallback(function (): Generator {
+            $this->expectException(InvalidOperationException::class);
+
+            yield $this->conn->createPersistentSubscriptionAsync(
+                $this->stream,
+                'group32',
+                $this->settings,
+                DefaultData::adminCredentials()
+            );
+        });
+    }
+}
