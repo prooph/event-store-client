6736f6803a3ef594e8ec03d2c6ea5fc0ba484cb6
prolic <saschaprolic@googlemail.com>
2018-10-28 23:43:47 +0800

    add tests

---
+++ tests/when_creating_transient_projection.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of `prooph/event-store-client`.
+ * (c) 2018-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2018-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStoreClient;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+use Prooph\EventStoreClient\Internal\UuidGenerator;
+use Throwable;
+
+class when_creating_transient_projection extends TestCase
+{
+    use ProjectionSpecification;
+
+    /** @var string */
+    private $projectionName;
+    /** @var string */
+    private $streamName;
+    /** @var string */
+    private $query;
+
+    public function given(): Generator
+    {
+        $id = UuidGenerator::generate();
+        $this->projectionName = 'when_creating_transient_projection-' . $id;
+        $this->streamName = 'test-stream-' . $id;
+
+        yield $this->postEvent($this->streamName, 'testEvent', '{"A": 1}');
+        yield $this->postEvent($this->streamName, 'testEvent', '{"A": 2}');
+
+        $this->query = $this->createStandardQuery($this->streamName);
+    }
+
+    protected function when(): Generator
+    {
+        yield $this->projectionsManager->createTransientAsync(
+            $this->projectionName,
+            $this->query,
+            'JS',
+            $this->credentials
+        );
+    }
+
+    /**
+     * @test
+     * @throws Throwable
+     */
+    public function should_create_projection(): void
+    {
+        $this->execute(function () {
+            $status = yield $this->projectionsManager->getStatusAsync(
+                $this->projectionName,
+                $this->credentials
+            );
+
+            $this->assertNotEmpty($status);
+        });
+    }
+}
