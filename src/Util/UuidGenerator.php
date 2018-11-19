ef81d00c4a207209d6e4173cef382883b569d5b6
prolic <saschaprolic@googlemail.com>
2018-11-19 22:31:11 +0800

    fix uuid generator

--- src/Util/UuidGenerator.php
+++ src/Util/UuidGenerator.php
@@ -13,26 +13,18 @@ declare(strict_types=1);
 
 namespace Prooph\EventStoreClient\Util;
 
+use Ramsey\Uuid\Uuid;
+
 class UuidGenerator
 {
     public static function generate(): string
     {
-        $uuidBin = \random_bytes(18);
-        $uuidBin &= "\xFF\xFF\xFF\xFF\x0F\xFF\xF0\x0F\xFF\x03\xFF\xF0\xFF\xFF\xFF\xFF\xFF\xFF";
-        $uuidBin |= "\x00\x00\x00\x00\x00\x00\x00\x40\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00";
-        $uuidHex = \bin2hex($uuidBin);
-        $uuidHex[8] = $uuidHex[13] = $uuidHex[18] = $uuidHex[23] = '-';
-
-        return $uuidHex;
+        return Uuid::uuid4()->toString();
     }
 
     public static function generateWithoutDash(): string
     {
-        $uuidBin = \random_bytes(16);
-        $uuidBin &= "\xFF\xFF\xFF\xFF\xFF\xFF\x0F\xFF\x3F\xFF\xFF\xFF\xFF\xFF\xFF\xFF";
-        $uuidBin |= "\x00\x00\x00\x00\x00\x00\x40\x00\x80\x00\x00\x00\x00\x00\x00\x00";
-
-        return \bin2hex($uuidBin);
+        return \str_replace('-', '', self::generate());
     }
 
     public static function empty(): string
